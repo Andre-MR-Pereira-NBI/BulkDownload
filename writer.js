a258973577db734809adaac7d20cbc1dec2c444d
Andre-MR-Pereira-NBI <andre.pereira@nbi.ie>
2023-11-09 16:51:51 +0000

    NPM and Python local servers

---
+++ writer.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json({limit: '500mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
+var endpoint = '/writePDF';
+
+app.post(endpoint, function(req, res) {
+
+    try{
+        console.log(req.body)
+        console.log(req.headers.Name)
+        var dir = './BulkDownload/';
+        var namePDF = req.headers.Name;
+    
+        fs.writeFileSync(dir + namePDF, JSON.stringify(req.body));
+        res.status(201).send('Saved to ' + dir + namePDF);
+    }catch(error){
+        console.log(error)
+        res.status(500).send('Failed to store file' + namePDF);
+    }
+
+});
+
+var port = 3000;
+app.listen(port);
+console.log("\nReady to write files on https://localhost:" + port + endpoint);
---
+++ writer.py
@@ -0,0 +1,35 @@
+import os
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from time import time
+
+dir = './BulkDownload/'
+
+class FileUploadHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_type = self.headers.get('Content-Type', 'application/json')
+        doc_name = self.headers.get('Name', 'temp')
+        content_length = int(self.headers.get('Content-Length', 0))
+        post_data = self.rfile.read(content_length)
+        if content_type == 'application/json':
+            post_data = json.loads(post_data)
+            file_extension = post_data.get('fileExtension', content_type.split('/')[1])
+            file_path = os.path.join(os.getcwd(), dir + doc_name)
+            with open(file_path, 'w') as f:
+                if file_extension == 'json':
+                  json.dump(json.loads(post_data.get('responseData')), f)
+                else:
+                  f.write(post_data)
+        else:
+          file_extension = content_type.split('/')[1]
+          file_path = os.path.join(os.getcwd(), dir + doc_name)
+          with open(file_path, 'wb') as f:
+              f.write(post_data)
+        self.send_response(201)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        self.wfile.write(bytes(json.dumps({'filePath': file_path}), 'utf8'))
+if __name__ == '__main__':
+    with HTTPServer(('', 3000), FileUploadHandler) as server:
+      print('Starting HTTP server...')
+      server.serve_forever()
