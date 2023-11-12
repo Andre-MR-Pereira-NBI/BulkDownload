5f4415f5f2efcb178e1c849d3f05681416969ac7
Andre-MR-Pereira-NBI <andre.pereira@nbi.ie>
2023-11-12 10:35:30 +0000

    Added Postman collection and Python endpoints

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # BulkDownload
-Bulk download files from The Wire from NBI
+Collection of Postman curl commands to run in a collection and a python REST server for the localhost to download all the files in the HR tab from The Wire.
+
+## Installation
+It is necessary to have Python installed, as well as both Postman app and CLI.
+
+It is also necessary to import the collection to run to Postman:
+
+- **In Postman app, go to Collections -> Import -> Select the file named BulkDocsWire.postman_collection.json in the git repository.**
+
+## Usage
+### Inputs
+**On the Post request "Login to Wire NBI":**
+>1. Change the \<NBI_EMAIL> field to your email.
+>2. Change the \<PASSWORD> field to your password.
+
+Like so:
+![alt text](Assets/Input.PNG)
+### Launch scripts
+#### Local Endpoint
+Open a CMD terminal (Windows Powershell, i.e) inside the git repository and run the command: **python writer.py**.
+Like so:
+![alt text](Assets/LocalEndpoint.PNG)
+Keep this terminal open while opening a different terminal.
+
+#### Postman
+In Postman app, go to Collections -> ... -> Run Collection:
+![alt text](Assets/ChooseCollection.PNG)
+In the newly opened tab, choose Functional -> Automate runs via CLI. In the tab beneath that pops up (Run on Postman CLI), there are 2 Postman CLI commands that will be necessary to run on the second terminal.
+![alt text](Assets/RunCollectionPostman.PNG)
+Therefore, it is necessary to generate a API Key by pressing the corresponding button. Afterwards, the commands should be in this format:
+>postman login --with-api-key <API_KEY_GENERATED>
+>postman collection run <COLLECTION_ID>
+
+These commands should then be copy and pasted on the new terminal window, as such:
+![alt text](Assets/RunCollectionCLI.PNG)
+
+#### Result
+A folder named ZipDocsExtracted.zip will appear after the end of the scripts runs, containing all the files extracted.
--- writer.py
+++ writer.py
@@ -1,34 +1,44 @@
 import os
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from time import time
+import base64
+import shutil
 
 dir = './DocsExtracted/'
+zipped_dir = './ZipDocsExtracted'
 
 class FileUploadHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        content_type = self.headers.get('Content-Type', 'application/json')
-        doc_name = self.headers.get('Name', 'temp')
-        content_length = int(self.headers.get('Content-Length', 0))
-        post_data = self.rfile.read(content_length)
-        if content_type == 'application/json':
-            post_data = json.loads(post_data)
-            file_extension = post_data.get('fileExtension', content_type.split('/')[1])
+        try:
+            doc_name = self.headers.get('Name', 'temp')
+            content_length = int(self.headers.get('Content-Length', 0))
+            post_data = self.rfile.read(content_length)
             file_path = os.path.join(os.getcwd(), dir + doc_name)
-            with open(file_path, 'w') as f:
-                if file_extension == 'json':
-                  json.dump(json.loads(post_data.get('responseData')), f)
-                else:
-                  f.write(post_data)
-        else:
-          file_extension = content_type.split('/')[1]
-          file_path = os.path.join(os.getcwd(), dir + doc_name)
-          with open(file_path, 'wb') as f:
-              f.write(post_data)
-        self.send_response(201)
-        self.send_header('Content-type','application/json')
-        self.end_headers()
-        self.wfile.write(bytes(json.dumps({'filePath': file_path}), 'utf8'))
+            with open(file_path, 'wb') as f:
+                decoded_data = base64.b64decode(post_data)
+                f.write(decoded_data)
+            self.send_response(201)
+            self.send_header('Content-type','application/json')
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps({'filePath': file_path}), 'utf8'))
+        except:
+            self.send_response(500)
+            self.send_header('Content-type','application/json')
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps({'filePath': file_path}), 'utf8'))
+    def do_GET(self):
+        try:
+            shutil.make_archive(zipped_dir, 'zip', dir)
+            self.send_response(200)
+            self.send_header('Content-type','application/json')
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps({'filePath': zipped_dir}), 'utf8'))
+        except:
+            self.send_response(500)
+            self.send_header('Content-type','application/json')
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps({'filePath': zipped_dir}), 'utf8'))
+
 if __name__ == '__main__':
     with HTTPServer(('', 3000), FileUploadHandler) as server:
       print('Starting HTTP server...')
